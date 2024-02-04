336e08b4df1c758b6e46ed68adad3106e9a2b099
Ahmed <ahmed.nabil1@nomadz.dev>
2024-02-04 22:02:03 +0200

    Finished bottom navigation in home screen. Added 4 tabs(quran - ahadeth - radio- sebha).

--- lib/ui/screens/home/home_screen.dart
+++ lib/ui/screens/home/home_screen.dart
@@ -1,28 +1,83 @@
 import 'package:flutter/material.dart';
+import 'package:islami_sun_c10/ui/screens/home/tabs/ahadeth/ahadeth_tab.dart';
+import 'package:islami_sun_c10/ui/screens/home/tabs/quran/quran_tab.dart';
+import 'package:islami_sun_c10/ui/screens/home/tabs/radio/radio_tab.dart';
+import 'package:islami_sun_c10/ui/screens/home/tabs/sebha/sebha_tab.dart';
 import 'package:islami_sun_c10/ui/utils/app_assets.dart';
 import 'package:islami_sun_c10/ui/utils/app_colors.dart';
 import 'package:islami_sun_c10/ui/utils/app_theme.dart';
 
-class HomeScreen extends StatelessWidget {
+class HomeScreen extends StatefulWidget {
   static const String routeName = "home screen";
+
   const HomeScreen({super.key});
+
+  @override
+  State<HomeScreen> createState() => _HomeScreenState();
+}
+
+class _HomeScreenState extends State<HomeScreen> {
+  int currentTabIndex = 0;
+  Widget body = QuranTab();
 
   @override
   Widget build(BuildContext context) {
     return Container(
       decoration: const BoxDecoration(
-        image: DecorationImage(image: AssetImage(AppAssets.background))
-      ),
+          image: DecorationImage(image: AssetImage(AppAssets.background))),
       child: Scaffold(
         backgroundColor: AppColors.transparent,
-        appBar: AppBar(
-          backgroundColor: AppColors.transparent,
-          centerTitle: true,
-          elevation: 0,
-          title: const Text("Islami", style: AppTheme.appBarTextStyle,),
-
-        ),
+        appBar: buildAppBar(),
+        bottomNavigationBar: buildBottomNavigation(),
+        body: body,
       ),
     );
   }
+
+  AppBar buildAppBar() => AppBar(
+        backgroundColor: AppColors.transparent,
+        centerTitle: true,
+        elevation: 0,
+        title: const Text(
+          "Islami",
+          style: AppTheme.appBarTextStyle,
+        ),
+      );
+
+  Widget buildBottomNavigation() => Theme(
+        data: ThemeData(canvasColor: AppColors.orange),
+        child: BottomNavigationBar(
+          backgroundColor: AppColors.orange,
+          items: [
+            buildBottomNavigationBarItem(AppAssets.icQuran, "Quran"),
+            buildBottomNavigationBarItem(AppAssets.icAhadeth, "Ahadeth"),
+            buildBottomNavigationBarItem(AppAssets.icSebha, "Sebha"),
+            buildBottomNavigationBarItem(AppAssets.icRadio, "Radio"),
+          ],
+          selectedItemColor: AppColors.lightBlack,
+          currentIndex: currentTabIndex,
+          onTap: (index) {
+            currentTabIndex = index;
+            if (currentTabIndex == 0) {
+              body = QuranTab();
+            } else if (currentTabIndex == 1) {
+              body = AhadethTab();
+            } else if (currentTabIndex == 2) {
+              body = SebhaTab();
+            } else {
+              body = RadioTab();
+            }
+            setState(() {});
+          },
+        ),
+      );
+
+  BottomNavigationBarItem buildBottomNavigationBarItem(
+          String imagePath, String label) =>
+      BottomNavigationBarItem(
+          icon: ImageIcon(
+            AssetImage(imagePath),
+            size: 32,
+          ),
+          label: label);
 }
--- lib/ui/utils/app_assets.dart
+++ lib/ui/utils/app_assets.dart
@@ -1,4 +1,10 @@
 abstract class AppAssets{
   static const String splash = "assets/images/splash.png";
   static const String background = "assets/images/main_background.png";
+  static const String icQuran = "assets/images/ic_quran.png";
+  static const String icAhadeth = "assets/images/ic_ahadeth.png";
+  static const String icSebha = "assets/images/ic_sebha.png";
+  static const String icRadio = "assets/images/ic_radio.png";
+  static const String quranTabLogo = "assets/images/quran_tab_logo.png";
+  static const String ahadethTabLogo = "assets/images/ahadeth_tab_logo.png";
 }
